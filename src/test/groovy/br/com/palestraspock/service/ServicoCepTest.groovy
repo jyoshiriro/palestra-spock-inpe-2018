20d6c3005e9a7a033d28b345f3e8e574bdcf945b
José Yoshiriro Ajisaka Ramos <jose.ramos@trescon.com.br>
2018-11-01 00:11:12 -0200

    Testes sem conteúdo

--- src/test/groovy/br/com/palestraspock/service/ServicoCepTest.groovy
+++ src/test/groovy/br/com/palestraspock/service/ServicoCepTest.groovy
@@ -1,39 +1,6 @@
 package br.com.palestraspock.service
 
-import br.com.palestraspock.servico.ClienteRestCep
-import br.com.palestraspock.servico.ServicoCep
-import spock.lang.Specification
+class ServicoCepTest {
 
-class ServicoCepTest extends Specification {
 
-    ClienteRestCep clienteRest = Mock()
-	
-    def 'Deve retornar o logradouro encontrado'() {
-        given:
-        def logradouro = 'Rua da Abobrinha'
-
-        clienteRest.getLogradouro(_) >> logradouro
-
-        def servico = new ServicoCep(cliente: clienteRest)
-
-        when:
-        def encontrado = servico.pesquisaLogradouro('04259688')
-
-        then:
-        encontrado == 'Rua de Abobrimha'
-    }
-
-    def 'Deve retornar mensagem de erro para falha de comunicação'() {
-        given:
-        clienteRest.getLogradouro(_) >> {
-            throw new IllegalStateException()
-        }
-        def servico = new ServicoCep(cliente: clienteRest)
-
-        when:
-        def encontrado = servico.pesquisaLogradouro('04259688')
-
-        then:
-        encontrado == "Falha de internet ou serviço indisponível"
-    }
 }
--- src/test/groovy/br/com/palestraspock/util/CalculadoraInssTest.groovy
+++ src/test/groovy/br/com/palestraspock/util/CalculadoraInssTest.groovy
@@ -3,50 +3,6 @@ package br.com.palestraspock.util
 import spock.lang.Specification
 
 
-class CalculadoraInssTest extends Specification {
+class CalculadoraInssTest {
 
-    def 'Salário de 0 não deve ter desconto'() {
-        given:
-        def salario = 0
-
-        when:
-        def desconto = new CalculadoraInss(salario).getDesconto()
-
-        then:
-        desconto == 0
-    }
-
-    def 'Salário de 1000 deve ter desconto de 50'() {
-        given:
-        def salario = 1000
-
-        when:
-        def desconto = new CalculadoraInss(salario).getDesconto()
-
-        then:
-        desconto == 50
-    }
-
-    def 'Salário não pode ser menor que 0'() {
-        given:
-        def salario = -1
-
-        when:
-        new CalculadoraInss(salario)
-
-        then:
-        thrown(IllegalArgumentException)
-    }
-
-    def 'Faixas de INSS são calculadas corretamente'() {
-        expect:
-        new CalculadoraInss(salario).getDesconto() == inss
-
-        where:
-        salario | inss
-        0       | 0
-        1000    | 50
-        2000    | 200
-        4000    | 800
-    }
 }
